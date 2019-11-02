175c2d3616511b9e531e1366615912252ee761e9
Oleksandr Bieliakov <abel9k@gmail.com>
2019-11-02 21:12:40 +0100

    PeopleGroups with extensions implemented

---
+++ src/ass6/groupscollections/Departments.java
@@ -0,0 +1,13 @@
+package ass6.groupscollections;
+
+import ass6.groups.Department;
+
+import java.util.Collection;
+
+public class Departments extends PeopleGroups<Department> {
+
+    public Departments(Collection<Department> departments) {
+        super(departments);
+    }
+
+}
---
+++ src/ass6/groupscollections/StudentsGroups.java
@@ -0,0 +1,13 @@
+package ass6.groupscollections;
+
+import ass6.groups.StudentsGroup;
+
+import java.util.Collection;
+
+public class StudentsGroups extends PeopleGroups<StudentsGroup> {
+
+    public StudentsGroups(Collection<StudentsGroup> studentsGroups) {
+        super(studentsGroups);
+    }
+
+}
